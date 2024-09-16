d44007bd51a6eddca6860dc55f1bd12282a352b7
samuelxe2 <samuelantoniosanchez13@gmail.com>
2024-09-15 20:03:02 -0500

    asembler codigo de una suma en un registro

---
+++ Assembler/suma.asm
@@ -0,0 +1,21 @@
+section .data
+    num1 db 10    ; Primer número a sumar
+    num2 db 20    ; Segundo número a sumar
+    sum db 0      ; Variable para almacenar el resultado
+
+section .text
+    global _start
+
+_start:
+    ; Cargar el primer número en el registro AL
+    mov al, [num1]
+
+    ; Sumar el segundo número al registro AL
+    add al, [num2]
+
+    ; Guardar el resultado en la variable sum
+    mov [sum], al
+
+    ; Salir del programa
+    mov eax, 1    ; Código de salida (sys_exit)
+    int 0x80      ; Llamar al kernel
